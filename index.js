563c27ccf886a52784558312c497b41e926261cc
David Pulloquinga <dapl@me.com>
2016-04-13 16:16:16 -0500

    ok

--- index.js
+++ index.js
@@ -2,13 +2,20 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === '123') {
         res.send(req.query['hub.challenge']);
     }
     res.send('Error, wrong validation token');
 })
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
